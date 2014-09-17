26759e2bb34504f8bc6f3c88f6d7842e0e27d4fe
Abdul Waheed Mulla <my.hoon.don@gmail.com>
2014-09-17 14:22:30 +0530

    navigation to deal detail is in place.

--- WebContent/contractsinamendment/view/Master.controller.js
+++ WebContent/contractsinamendment/view/Master.controller.js
@@ -3,13 +3,13 @@ jQuery.sap.require("dia.cmc.common.helper.CommonController");
 jQuery.sap.require("sap.ca.ui.dialog.factory");
 sap.ui.controller("dia.cmc.contractsinamendment.view.Master", {
     onInit: function() {
-    
-    	// Model Helper reference
-		this.ModelHelper = dia.cmc.common.helper.ModelHelper;		
 
-		// Common Controller reference
-		this.CommonController = dia.cmc.common.helper.CommonController;
-		
+        // Model Helper reference
+        this.ModelHelper = dia.cmc.common.helper.ModelHelper;
+
+        // Common Controller reference
+        this.CommonController = dia.cmc.common.helper.CommonController;
+
         // set i18n model
         var i18nModel = new sap.ui.model.resource.ResourceModel({
             bundleUrl: "contractsinamendment/i18n/messageBundle.properties"
@@ -20,24 +20,24 @@ sap.ui.controller("dia.cmc.contractsinamendment.view.Master", {
         var oModel = new sap.ui.model.json.JSONModel("contractsinamendment/model/products.json");
         this.getView().setModel(oModel);
 
-      /*  var oComp = sap.ui.getCore().createComponent({
-            name: 'dia.cmc.contractsinamendment.tableview'
-        });
-        oComp.setModel(this.getView().getModel());
-        this._oTable = oComp.getTable();
-        this.getView().byId("idIconTabBar").insertContent(this._oTable);*/
-        
-      /*  var bus = sap.ui.getCore().getEventBus();
-        bus.subscribe("nav", "to", sap.ui.controller("dia.cmc.contractsinamendment.view.App").navToHandler, this);
-        bus.subscribe("nav", "back", sap.ui.controller("dia.cmc.contractsinamendment.view.App").navBackHandler, this);*/
-        $(document).ready(function(){    
+        /*  var oComp = sap.ui.getCore().createComponent({
+              name: 'dia.cmc.contractsinamendment.tableview'
+          });
+          oComp.setModel(this.getView().getModel());
+          this._oTable = oComp.getTable();
+          this.getView().byId("idIconTabBar").insertContent(this._oTable);*/
+
+        /*  var bus = sap.ui.getCore().getEventBus();
+          bus.subscribe("nav", "to", sap.ui.controller("dia.cmc.contractsinamendment.view.App").navToHandler, this);
+          bus.subscribe("nav", "back", sap.ui.controller("dia.cmc.contractsinamendment.view.App").navBackHandler, this);*/
+        $(document).ready(function() {
             $("#__xmlview1--idIconTabBar-content").remove();
         });
 
     },
 
     handleIconTabBarSelect: function(oEvent) {
-    	this._oTable = this.getView().byId("idTable");
+        this._oTable = this.getView().byId("idTable");
         var oBinding = this._oTable.getBinding("items"),
             sKey = oEvent.getParameter("selectedKey"),
             oFilter;
@@ -56,16 +56,31 @@ sap.ui.controller("dia.cmc.contractsinamendment.view.Master", {
         }
 
     },
-    handleLineItemPress: function(evt) {
+    /**
+     * Navigate to Deal Detail.
+     */
+    handleLineItemPress: function(oEvent) {
         console.log("This will navigate to details page");
+        var oContext = oEvent.getSource().getBindingContext();
+        // set selected Deal path to helper class
+        this.ModelHelper.sSelectedDealPathIndex = oContext.getPath();
+
+        // If we're on a phone, include nav in history; if not, don't.
+        var bReplace = jQuery.device.is.phone ? false : true;
+
+        var oDealDetail = this.getView().getModel().getProperty(oContext.getPath());
+
+        this.CommonController.getRouter(this).navTo("dealDetail", {
+            from: "master",
+            dealId: oDealDetail.DealId,
+        }, bReplace);
     },
-    
-    
-    
+
+
     handleCancelPress: function(evt) {
-        
-    	console.log("{i18n>CancelAmendment}");
-        
+
+        console.log("{i18n>CancelAmendment}");
+
         var fnClose = function(oResult) {
             if (oResult) {
                 /*console.log("isConfirmed:" + oResult.isConfirmed);
@@ -74,60 +89,55 @@ sap.ui.controller("dia.cmc.contractsinamendment.view.Master", {
                 }*/
             }
         }
-        
-      //open the confirmation dialog
+
+        //open the confirmation dialog
         sap.ca.ui.dialog.confirmation.open({
-            question : "Would you like to cancel selected amendment ?",            
-            title : "Cancel Amendment",
-            confirmButtonLabel : "Ok"
+            question: "Would you like to cancel selected amendment ?",
+            title: "Cancel Amendment",
+            confirmButtonLabel: "Ok"
         }, fnClose);
     },
-   
+
     /**
      * Navigate to Amendment Flow.
      */
-    handleWorkFlowPress : function(oEvent) {	
-    	
-    	var oContext = oEvent.getSource().getBindingContext();
-		this._displayDealDetail(oContext);	 
-	},
-	/**
-	 * Display pop-up.
-	 */
-	handlePopoverPress : function(oEvent) {
-       var local = oEvent.getParameters();
-	   var lastChar = local.id;
-	   lastChar = lastChar.substr(lastChar.length - 1);
-			
-	    // create popover
-		// if (!this._oPopover) {
-		this._oPopover = sap.ui.xmlfragment(
-					"dia.cmc.contractsinamendment.fragments.AmendmentDescription", this);
-		this.getView().addDependent(this._oPopover);
-		this._oPopover.bindElement("/AmendmentsCollection/" + lastChar);
-		// }
-			
-		var oButton = oEvent.getSource();
-		jQuery.sap.delayedCall(0, this, function() {
-		this._oPopover.openBy(oButton);
-		});
-		},
-	_displayDealDetail: function(oContext){
-		// set selected Deal path to helper class
-		this.ModelHelper.sSelectedDealPathIndex = oContext.getPath();
-		
-		// If we're on a phone, include nav in history; if not, don't.
-		var bReplace = jQuery.device.is.phone ? false : true;
-		
-		var oDealDetail = this.getView().getModel().getProperty(oContext.getPath());
-		
-		this.CommonController.getRouter(this).navTo("AmendmentFlow", {
-			from: "master",
-			dealId: oDealDetail.DealId,
-		}, bReplace);
-		
-		
-	},
-    
+    handleWorkFlowPress: function(oEvent) {
+
+        var oContext = oEvent.getSource().getBindingContext();
+
+        this.ModelHelper.sSelectedDealPathIndex = oContext.getPath();
+
+        // If we're on a phone, include nav in history; if not, don't.
+        var bReplace = jQuery.device.is.phone ? false : true;
+
+        var oDealDetail = this.getView().getModel().getProperty(oContext.getPath());
+
+        this.CommonController.getRouter(this).navTo("AmendmentFlow", {
+            from: "master",
+            dealId: oDealDetail.DealId,
+        }, bReplace);
+    },
+    /**
+     * Display pop-up.
+     */
+    handlePopoverPress: function(oEvent) {
+        var local = oEvent.getParameters();
+        var lastChar = local.id;
+        lastChar = lastChar.substr(lastChar.length - 1);
+
+        // create popover
+        // if (!this._oPopover) {
+        this._oPopover = sap.ui.xmlfragment(
+            "dia.cmc.contractsinamendment.fragments.AmendmentDescription", this);
+        this.getView().addDependent(this._oPopover);
+        this._oPopover.bindElement("/AmendmentsCollection/" + lastChar);
+        // }
+
+        var oButton = oEvent.getSource();
+        jQuery.sap.delayedCall(0, this, function() {
+            this._oPopover.openBy(oButton);
+        });
+    },
+
 
 });
