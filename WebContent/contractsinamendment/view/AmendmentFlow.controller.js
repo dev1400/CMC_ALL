609bc26b3bf12ae8cd027988c7982ef99f2ca736
Abdul Waheed Mulla <my.hoon.don@gmail.com>
2014-10-29 16:51:32 +0530

    d

--- WebContent/contractsinamendment/view/AmendmentFlow.controller.js
+++ WebContent/contractsinamendment/view/AmendmentFlow.controller.js
@@ -5,27 +5,27 @@ sap.ui.controller("dia.cmc.contractsinamendment.view.AmendmentFlow", {
 	onInit: function () {		
 		// Common Controller reference
 		this.CommonController = dia.cmc.common.helper.CommonController;
+		this.ModelHelper = dia.cmc.common.helper.ModelHelper;
 		
-	    var oDataProcessFlowLanesOnly = {
+	    var oProcessFlowLanes = {
 	        lanes:
 	          [
-	            {id: "0", icon: "sap-icon://order-status", label: "In Order", position: 0},
-	            {id: "1", icon: "sap-icon://monitor-payments", label: "In Delivery", position: 1},
-	            {id: "2", icon: "sap-icon://payment-approval", label: "In Invoice", position: 2},
-	            {id: "3", icon: "sap-icon://money-bills", label: "In Accounting", position: 3},
-	            {id: "4", icon: "sap-icon://payment-approval", label: "In Payment", position: 4},
-	            {id: "5", icon: "sap-icon://nurse", label: "Delivered", position: 5},
-	            /*{id: "6", icon: "sap-icon://retail-store", label: "In Return Process", position: 6},
-	            {id: "7", icon: "sap-icon://monitor-payments", label: "In Credit Return", position: 7}*/
+	          {id: "0", icon: "sap-icon://order-status", label: this.ModelHelper.getText("Proposal"), position: 0},
+	          {id: "1", icon: "sap-icon://monitor-payments", label: this.ModelHelper.getText("ExecutionPreparation"), position: 1},
+	          {id: "2", icon: "sap-icon://payment-approval", label: this.ModelHelper.getText("ExecutionHandling"), position: 2},
+	          {id: "3", icon: "sap-icon://money-bills", label: this.ModelHelper.getText("ExecutionClosure"), position: 3},
+	          {id: "4", icon: "sap-icon://payment-approval", label: this.ModelHelper.getText("DocumentationAndCleanUp"), position: 4},
+
+
 	          ]
 	    };
 
 	    var oModel = new sap.ui.model.json.JSONModel();
 	    var view = this.getView();
-	    oModel.setData(oDataProcessFlowLanesOnly);
-	    view.setModel(oModel, "pf3");
+	    oModel.setData(oProcessFlowLanes);
+	    view.setModel(oModel, "processFlow");
 
-	    view.byId("processflow3").updateModel();
+	    view.byId("idProcessFlow").updateModel();
 	  },
 	  
 	  /**
@@ -40,36 +40,38 @@ sap.ui.controller("dia.cmc.contractsinamendment.view.AmendmentFlow", {
 	    var oDataProcessFlowNodes = {
 	        nodes:
 	        [
-	          {id: "1",  lane: "0",  title: "Sales Order 2", children: [10, 11, 12], state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, stateText: "OK status", focused: true, texts: ["Sales Order Document Overdue long text for the wrap up all the aspects", "Not cleared"]},
-	          {id: "10", lane: "1" , title: "Outbound Delivery 40", children: [20, 21], state: sap.suite.ui.commons.ProcessFlowNodeState.Negative, stateText: "NOT OK", texts: ["text 1", "text 2"]},
+              {id: "0", lane: "0" , title: "Deal Configuration", children: null, state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, stateText: "OK status"},
+              {id: "1", lane: "1" , title: "Deal Approval", children: null, state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, stateText: "OK status"},
+ 	         
+
+	         /* {id: "1",  lane: "0",  title: "Deal Configuration", children: [2], state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, stateText: "OK status", focused: true, texts: ["Sales Order Document Overdue long text for the wrap up all the aspects", "Not cleared"]},
+	          {id: "2",  lane: "1",  title: "Deal Approval", children: null, state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, stateText: "OK status", focused: true, texts: ["Sales Order Document Overdue long text for the wrap up all the aspects", "Not cleared"]},
+	          */
+	         /* {id: "10", lane: "1" , title: "Outbound Delivery 40", children: [20, 21], state: sap.suite.ui.commons.ProcessFlowNodeState.Negative, stateText: "NOT OK", texts: ["text 1", "text 2"]},
 	          {id: "11", lane: "1" , title: "Outbound Delivery 43", children: [20], texts: ["text 1", "text 2"]},
 	          {id: "12", lane: "1" , title: "Outbound Delivery 45", children: [20]},
-	          {id: "20",  lane: "2" , title: "Invoice 9",  children: null /*[31, 51], state: sap.suite.ui.commons.ProcessFlowNodeState.Positive*/, stateText: "OK status"},
-	          {id: "21", lane: "2" , title: "Invoice planned", children: null, state: sap.suite.ui.commons.ProcessFlowNodeState.Planned},
-	         /* {id: "31",  lane: "3" , title: "Accounting Document 7",  children: [41], state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, stateText: "OK status"},
+	         */ /*{id: "20",  lane: "2" , title: "Invoice 9",  children: null [31, 51], state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, stateText: "OK status"},
+	         */ /* {id: "31",  lane: "3" , title: "Accounting Document 7",  children: [41], state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, stateText: "OK status"},
 	          {id: "41",  lane: "4" , title: "Payment Document 75",  children: [51], state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, stateText: "OK status"},
 	          {id: "51",  lane: "5" , title: "Acceptance Letter 14",  children: [61], state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, stateText: "OK status"},
 	          {id: "61",  lane: "6" , title: "Credit Voucher 67",  children: [71], state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, stateText: "OK status"},
 	          {id: "71",  lane: "7" , title: "Credit Return 77",  children: null, state: sap.suite.ui.commons.ProcessFlowNodeState.Planned, stateText: "Planned status text"}
 	      */  ],
 	        lanes:
-	        [
-	          {id: "0", icon: "sap-icon://order-status", label: "In Order", position: 0},
-	          {id: "1", icon: "sap-icon://monitor-payments", label: "In Delivery", position: 1},
-	          {id: "2", icon: "sap-icon://payment-approval", label: "In Invoice", position: 2},
-	          {id: "3", icon: "sap-icon://money-bills", label: "In Accounting", position: 3},
-	          {id: "4", icon: "sap-icon://payment-approval", label: "In Payment", position: 4},
-	          {id: "5", icon: "sap-icon://nurse", label: "Delivered", position: 5},
-	         /* {id: "6", icon: "sap-icon://retail-store", label: "In Return Process", position: 6},
-	          {id: "7", icon: "sap-icon://monitor-payments", label: "In Credit Return", position: 7}*/
-	        ]
+	        	[
+		          {id: "0", icon: "sap-icon://order-status", label: this.ModelHelper.getText("Proposal"), position: 0},
+		          {id: "1", icon: "sap-icon://monitor-payments", label: this.ModelHelper.getText("ExecutionPreparation"), position: 1},
+		          {id: "2", icon: "sap-icon://payment-approval", label: this.ModelHelper.getText("ExecutionHandling"), position: 2},
+		          {id: "3", icon: "sap-icon://money-bills", label: this.ModelHelper.getText("ExecutionClosure"), position: 3},
+		          {id: "4", icon: "sap-icon://payment-approval", label: this.ModelHelper.getText("DocumentationAndCleanUp"), position: 4},
+		        ]
 	      };
 
 	        var oModel = new sap.ui.model.json.JSONModel();
 	        var view = this.getView();
 	        oModel.setData(oDataProcessFlowNodes);
-	        view.setModel(oModel, "pf3");
-	        view.byId("processflow3").updateModel();
+	        view.setModel(oModel, "processFlow");
+	        view.byId("idProcessFlow").updateModel();
 	  },
 
 	  onNodePress: function(event) {
