89b27de2a0ed6f00bcc3699a74e2fb4009b784a4
Abdul Waheed Mulla <my.hoon.don@gmail.com>
2014-10-29 11:49:34 +0530

    needed

--- WebContent/processworkflow/view/ProcessFlow.controller.js
+++ WebContent/processworkflow/view/ProcessFlow.controller.js
@@ -1,38 +1,107 @@
-sap.ui.controller("processworkflow.view.ProcessFlow", {
+jQuery.sap.require("dia.cmc.common.helper.CommonController");
+sap.ui.controller("dia.cmc.processworkflow.view.ProcessFlow", {
+	onInit: function () {		
+		// Common Controller reference
+		this.CommonController = dia.cmc.common.helper.CommonController;
+		
+	    var oDataProcessFlowLanesOnly = {
+	        lanes:
+	          [
+	            {id: "0", icon: "sap-icon://order-status", label: "In Order", position: 0},
+	            {id: "1", icon: "sap-icon://monitor-payments", label: "In Delivery", position: 1},
+	            {id: "2", icon: "sap-icon://payment-approval", label: "In Invoice", position: 2},
+	            {id: "3", icon: "sap-icon://money-bills", label: "In Accounting", position: 3},
+	            {id: "4", icon: "sap-icon://payment-approval", label: "In Payment", position: 4},
+	            {id: "5", icon: "sap-icon://nurse", label: "Delivered", position: 5},
+	            /*{id: "6", icon: "sap-icon://retail-store", label: "In Return Process", position: 6},
+	            {id: "7", icon: "sap-icon://monitor-payments", label: "In Credit Return", position: 7}*/
+	          ]
+	    };
 
-/**
-* Called when a controller is instantiated and its View controls (if available) are already created.
-* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-* @memberOf processworkflow.view.ProcessFlow
-*/
-//	onInit: function() {
-//
-//	},
+	    var oModel = new sap.ui.model.json.JSONModel();
+	    var view = this.getView();
+	    oModel.setData(oDataProcessFlowLanesOnly);
+	    view.setModel(oModel, "pf3");
 
-/**
-* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-* (NOT before the first rendering! onInit() is used for that one!).
-* @memberOf processworkflow.view.ProcessFlow
-*/
-//	onBeforeRendering: function() {
-//
-//	},
+	    view.byId("processflow3").updateModel();
+	  },
+	  
+	  /**
+	   * Go back to main page
+	   */
+	  handleNavButtonPress : function (evt) {
+		this.CommonController.getRouter(this).myNavBack("main");
+	  },
 
-/**
-* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-* This hook is the same one that SAPUI5 controls get after being rendered.
-* @memberOf processworkflow.view.ProcessFlow
-*/
-//	onAfterRendering: function() {
-//
-//	},
+	  onHeaderPress: function( event ) {
+		 
+	    var oDataProcessFlowNodes = {
+	        nodes:
+	        [
+	          {id: "1",  lane: "0",  title: "Sales Order 2", children: [10, 11, 12], state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, stateText: "OK status", focused: true, texts: ["Sales Order Document Overdue long text for the wrap up all the aspects", "Not cleared"]},
+	          {id: "10", lane: "1" , title: "Outbound Delivery 40", children: [20, 21], state: sap.suite.ui.commons.ProcessFlowNodeState.Negative, stateText: "NOT OK", texts: ["text 1", "text 2"]},
+	          {id: "11", lane: "1" , title: "Outbound Delivery 43", children: [20], texts: ["text 1", "text 2"]},
+	          {id: "12", lane: "1" , title: "Outbound Delivery 45", children: [20]},
+	          {id: "20",  lane: "2" , title: "Invoice 9",  children: null /*[31, 51], state: sap.suite.ui.commons.ProcessFlowNodeState.Positive*/, stateText: "OK status"},
+	          {id: "21", lane: "2" , title: "Invoice planned", children: null, state: sap.suite.ui.commons.ProcessFlowNodeState.Planned},
+	         /* {id: "31",  lane: "3" , title: "Accounting Document 7",  children: [41], state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, stateText: "OK status"},
+	          {id: "41",  lane: "4" , title: "Payment Document 75",  children: [51], state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, stateText: "OK status"},
+	          {id: "51",  lane: "5" , title: "Acceptance Letter 14",  children: [61], state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, stateText: "OK status"},
+	          {id: "61",  lane: "6" , title: "Credit Voucher 67",  children: [71], state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, stateText: "OK status"},
+	          {id: "71",  lane: "7" , title: "Credit Return 77",  children: null, state: sap.suite.ui.commons.ProcessFlowNodeState.Planned, stateText: "Planned status text"}
+	      */  ],
+	        lanes:
+	        [
+	          {id: "0", icon: "sap-icon://order-status", label: "In Order", position: 0},
+	          {id: "1", icon: "sap-icon://monitor-payments", label: "In Delivery", position: 1},
+	          {id: "2", icon: "sap-icon://payment-approval", label: "In Invoice", position: 2},
+	          {id: "3", icon: "sap-icon://money-bills", label: "In Accounting", position: 3},
+	          {id: "4", icon: "sap-icon://payment-approval", label: "In Payment", position: 4},
+	          {id: "5", icon: "sap-icon://nurse", label: "Delivered", position: 5},
+	         /* {id: "6", icon: "sap-icon://retail-store", label: "In Return Process", position: 6},
+	          {id: "7", icon: "sap-icon://monitor-payments", label: "In Credit Return", position: 7}*/
+	        ]
+	      };
 
-/**
-* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-* @memberOf processworkflow.view.ProcessFlow
-*/
-//	onExit: function() {
-//
-//	}
+	        var oModel = new sap.ui.model.json.JSONModel();
+	        var view = this.getView();
+	        oModel.setData(oDataProcessFlowNodes);
+	        view.setModel(oModel, "pf3");
+	        view.byId("processflow3").updateModel();
+	  },
+
+	  onNodePress: function(event) {
+		  console.log("onNodePress");
+	    var textToDisplay = "Node ";
+	    textToDisplay += event.getParameters().getNodeId();
+	    textToDisplay += " was clicked";
+	    sap.m.MessageToast.show(textToDisplay);
+	  },
+
+	  onNodeTitlePress: function(event) {
+		  console.log("onNodeTitlePress");
+	    var textToDisplay = 'Node title "';
+	    textToDisplay += event.getParameters().getTitle();
+	    textToDisplay += '" was clicked';
+	    sap.m.MessageToast.show(textToDisplay);
+	  },
+
+	  onZoomIn: function () {
+	      this.getView().byId("processflow1").zoomIn();
+	      this.getView().byId("processflow1").getZoomLevel();
+
+	      sap.m.MessageToast.show("Zoom level changed to: " + this.getView().byId("processflow1").getZoomLevel());
+	  },
+
+	  onZoomOut: function () {
+	      this.getView().byId("processflow1").zoomOut();
+	      this.getView().byId("processflow1").getZoomLevel();
+
+	      sap.m.MessageToast.show("Zoom level changed to: " + this.getView().byId("processflow1").getZoomLevel());
+	  },
+
+	 
+	
+
 
 });
