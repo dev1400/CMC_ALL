10a1baf51608709d2e056568acb493351529606c
Abdul Waheed Mulla <my.hoon.don@gmail.com>
2014-10-30 11:15:50 +0530

    needed

--- WebContent/contractsinamendment/view/AmendmentFlow.controller.js
+++ WebContent/contractsinamendment/view/AmendmentFlow.controller.js
@@ -40,24 +40,24 @@ sap.ui.controller("dia.cmc.contractsinamendment.view.AmendmentFlow", {
 	           [           
                 {id: "1",  lane: "0",  title: "Release for Execution", children: [12, 13, 14], state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, stateText: "OK status", focused: true, texts: ["Sales Order Document Overdue long text for the wrap up all the aspects", "Not cleared"]},
                 
-	            {id: "12", lane: "1" , title: "Deal Acceptance", children: null},
-	            {id: "13", lane: "1" , title: "Price Update", children: null},
+	            {id: "12", lane: "1" , title: "Deal Acceptance", state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, children: null},
+	            {id: "13", lane: "1" , title: "Price Update", state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, children: null},
 	            {id: "14", lane: "1" , title: "Key Date determination", children: [15, 16, 17, 18]},
 	            
-	            {id: "15", lane: "2" , title: "Instrument Shipping",  children: null, stateText: "OK status"},
-	            {id: "16", lane: "2" , title: "Installation Scheduled", children: null},
-	            {id: "17", lane: "2" , title: "Installation Done", children: null},
-	            {id: "18", lane: "2" , title: "De-Installation Done", children: [19, 20, 21, 22, 23]},
+	            {id: "15", lane: "2" , title: "Instrument Shipping", state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, children: null, stateText: "OK status"},
+	            {id: "16", lane: "2" , title: "Installation Scheduled", state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, children: null},
+	            {id: "17", lane: "2" , title: "Installation Done", state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, children: null},
+	            {id: "18", lane: "2" , title: "De-Installation Done", state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, children: [19, 20, 21, 22, 23]},
 	            
-	            {id: "19", lane: "3" , title: "Deal structure closure", children: null},
-	            {id: "20", lane: "3" , title: "Service Amendments", children: null},
-	            {id: "21", lane: "3" , title: "Rental Amendments", children: null},
-	            {id: "22", lane: "3" , title: "Capitalization Done", children: null},
-	            {id: "23", lane: "3" , title: "Revenue Recognition Done", children: [24, 25, 26]},
+	            {id: "19", lane: "3" , title: "Deal structure closure", state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, children: null},
+	            {id: "20", lane: "3" , title: "Service Amendments", state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, children: null},
+	            {id: "21", lane: "3" , title: "Rental Amendments", state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, children: null},
+	            {id: "22", lane: "3" , title: "Capitalization Done", state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, children: null},
+	            {id: "23", lane: "3" , title: "Revenue Recognition Done", state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, children: [24, 25, 26]},
 	            
-	            {id: "24", lane: "4" , title: "Agreement Update in Agiloft", children: null},
-	            {id: "25", lane: "4" , title: "Legal closure", children: null},
-	            {id: "26", lane: "4" , title: "Amendment closure", children: null},
+	            {id: "24", lane: "4" , title: "Agreement Update in Agiloft", state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, children: null},
+	            {id: "25", lane: "4" , title: "Legal closure", state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, children: null},
+	            {id: "26", lane: "4" , title: "Amendment closure", state: sap.suite.ui.commons.ProcessFlowNodeState.Positive, children: null},
 	           ],
 	        lanes:
 	        	[
