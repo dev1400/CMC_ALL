dcfe6503c8cbfa7214dda200f00c43bf6e565d04
Abdul Waheed Mulla <my.hoon.don@gmail.com>
2014-11-10 16:43:44 +0530

    needed

--- WebContent/contractsinamendment/view/Master.controller.js
+++ WebContent/contractsinamendment/view/Master.controller.js
@@ -303,13 +303,17 @@ sap.ui.controller("dia.cmc.contractsinamendment.view.Master", {
      */
     handleDownLoadToExcelButtonPress: function(){
     	
-    	console.log(this.ModelHelper.readDealsInAmendmentCollection().getJSON());
-    	
-    	/*this.JSONToCSVConvertor(this.ModelHelper.readDealsInAmendmentCollection().getJSON(), "Report", true);*/
+      /*console.log(this.ModelHelper.readDealsInAmendmentCollection().getProperty("/DealsInAmendmentCollection"));*/    
+      this.JSONToCSVConvertor(this.ModelHelper.readDealsInAmendmentCollection().getProperty("/DealsInAmendmentCollection"), 
+    		  this.ModelHelper.getText("ContractsInAmendmentReport"), true);
     	
     },
+    /**
+     * Convert JSON data to Excel file. 
+     */
+
     JSONToCSVConvertor: function(JSONData, ReportTitle, ShowLabel) {
-	    //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
+	    //If JSONData is not an object then JSON.parser will parse the JSON string in an Object
 	    var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
 	    
 	    var CSV = '';    
@@ -359,19 +363,14 @@ sap.ui.controller("dia.cmc.contractsinamendment.view.Master", {
 	    //this will remove the blank-spaces from the title and replace it with an underscore
 	    fileName += ReportTitle.replace(/ /g,"_");   
 	    
-	    //Initialize file format you want csv or xls
+	    //Initialize file format we want csv or xls
 	    var uri = 'data:text/csv;charset=utf-8,' + escape(CSV);
 	    
-	    // Now the little tricky part.
-	    // you can use either>> window.open(uri);
-	    // but this will not work in some browsers
-	    // or you will not get the correct file extension    
 	    
-	    //this trick will generate a temp <a /> tag
 	    var link = document.createElement("a");    
 	    link.href = uri;
 	    
-	    //set the visibility hidden so it will not effect on your web-layout
+	    //set the visibility hidden so it will not effect on  web-layout
 	    link.style = "visibility:hidden";
 	    link.download = fileName + ".csv";
 	    
