4c99a054aa34c7fa890cf57bf159ac340c6eb6f4
Abdul Waheed Mulla <my.hoon.don@gmail.com>
2014-11-12 17:30:34 +0530

    n

--- WebContent/common/helper/CommonController.js
+++ WebContent/common/helper/CommonController.js
@@ -174,5 +174,90 @@ dia.cmc.common.helper.CommonController = {
 		oControl.setTooltip(oRichTooltip);
 		// return the control itself (makes this function a decorator function)
 		return oControl;
-	}
+	},
+	/**
+     * Generate Excel file using JSON data.
+     */
+
+    JSONToCSVConvertor: function(JSONData, ReportTitle, ShowLabel) {
+        //If JSONData is not an object then JSON.parser will parse the JSON string in an Object
+        var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+
+        var CSV = '';
+        //Set Report title in first row or line
+
+        CSV += ReportTitle + '\r\n\n';
+
+        //This condition will generate the Label/Header
+        if (ShowLabel) {
+            var row = "";
+
+            //This loop will extract the label from 1st index of on array
+            for (var index in arrData[0]) {
+            	//temporarily removing mentioned columns as they are empty
+               if (!([ '__metadata', 'Message', 'MessageType', 'EndDate', 'SalesOrg' , 'DistChannel' , 'Division' ].indexOf( index ) > -1) ){
+                //Now convert each value to string and comma-separated
+                row += index + ',';
+            	}
+            }
+
+            row = row.slice(0, -1);
+
+            //append Label row with line break
+            CSV += row + '\r\n';
+        }
+
+        //1st loop is to extract each row
+        for (var i = 0; i < arrData.length; i++) {
+            var row = "";
+
+            //2nd loop will extract each column and convert it in string comma-seprated
+            for (var index in arrData[i]) {
+            	
+            	if(!([ '__metadata', 'Message', 'MessageType', 'EndDate', 'SalesOrg' , 'DistChannel' , 'Division' ].indexOf( index ) > -1) ){
+            		if(arrData[i][index] === "CRTD"){
+            			 row += '"Created",';
+            		}else if(arrData[i][index] === "RELE"){
+            			row += '"Released",';
+            		}else if(arrData[i][index] === "EXEC"){
+            			row += '"Executed",';
+            		}else{
+            			 row += '"' + arrData[i][index] + '",';
+            		}  
+               
+            	}
+            }
+
+            row.slice(0, row.length - 1);
+
+            //add a line break after each row
+            CSV += row + '\r\n';
+        }
+
+        if (CSV == '') {
+            alert("Invalid data");
+            return;
+        }
+
+        //Generate a file name
+        var fileName = "_";
+        //this will remove the blank-spaces from the title and replace it with an underscore
+        fileName += ReportTitle.replace(/ /g, "_");
+
+        //Initialize file format we want csv or xls
+        var uri = 'data:text/csv;charset=utf-8,' + escape(CSV);
+
+
+        var link = document.createElement("a");
+        link.href = uri;
+
+        //set the visibility hidden so it will not effect on  web-layout
+        link.style = "visibility:hidden";
+        link.download = fileName + ".csv";
+
+        //this part will append the anchor tag and remove it after automatic click
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
 };
